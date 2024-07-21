721c18e5daae35e3b1b1cfef298507e4f46090da
deepesh <rddesign64@gmail.com>
2024-07-21 21:49:44 +0545

    vercel json

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,13 @@ const app = express();
 const port: number = process.env.API_PORT ? parseInt(process.env.API_PORT) : 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:["http://deploy-mern-1whp.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 // connect Database
 connectDb();
