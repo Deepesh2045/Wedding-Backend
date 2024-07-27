1c6f9a2506d78fe08c8b67c6bff13ac47dc33b75
deepesh <rddesign64@gmail.com>
2024-07-27 16:23:54 +0545

    origin delete

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const connectDB_1 = __importDefault(require("./connectDB"));
 const wedding_routes_1 = __importDefault(require("./Wedding/wedding.routes"));
 const app = (0, express_1.default)();
@@ -12,11 +11,13 @@ const port = process.env.API_PORT
     ? parseInt(process.env.API_PORT)
     : 3000;
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({
-    origin: "http://wedding-frontend-phi.vercel.app",
-    methods: ["POST", "GET"],
-    credentials: true,
-}));
+app.use(
+// cors({
+//   origin: "http://wedding-frontend-phi.vercel.app",
+//   methods: ["POST", "GET"],
+//   credentials: true,
+// })
+);
 // connect Database
 (0, connectDB_1.default)();
 // Register Routes
